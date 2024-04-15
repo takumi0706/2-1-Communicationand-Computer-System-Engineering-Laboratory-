b556a0e3a8d362f4b5081bacac158934ad6fe282
takumi0706 <ganndamu0706@gmail.com>
2024-04-15 14:30:20 +0900

    commit for task03

--- src/Labo2Task02.java
+++ src/Labo2Task02.java
@@ -1,2 +1,61 @@
+import java.util.Scanner;
+
 public class Labo2Task02 {
+    public static void main(String[] args) {
+        complex z1;
+        complex z2;
+
+//        数値の入力
+        System.out.println("Please input real and imaginary part of the first complex number. ex:2.0 3.0");
+        Scanner scanner = new Scanner(System.in);
+        double real1 = scanner.nextDouble();
+        double imag1 = scanner.nextDouble();
+        z1 = new complex(real1, imag1);
+        System.out.println("Z1 = "+z1);
+        System.out.println("Please input real and imaginary part of the second complex number. ex:2.0 3.0");
+        double real2 = scanner.nextDouble();
+        double imag2 = scanner.nextDouble();
+        z2 = new complex(real2, imag2);
+        System.out.println("Z2 = "+z2);
+
+        System.out.println("Z1 + Z2 = "+z1.plus(z2));
+        System.out.println("Z1 - Z2 = "+z1.minus(z2));
+        System.out.println("Z1 * Z2 = "+z1.mul(z2));
+        System.out.println("Z1 / Z2 = "+z1.div(z2));
+
+
+    }
+    static class complex{
+        public double real;
+        public double imag;
+
+        public complex(double real, double imag){
+            this.real = real;
+            this.imag = imag;
+        }
+//        絶対値を取得
+        public double abs(){
+            return Math.sqrt(real*real + imag*imag);
+        }
+//        複素数の表示方を成型
+        public String toString(){
+            return "("+real+ "," + imag + ")";
+        }
+//        足し算
+        public complex plus(complex other){
+            return new complex(real + other.real, imag+other.imag);
+        }
+//        引き算
+        public complex minus(complex other){
+            return new complex(real - other.real, imag - other.imag);
+        }
+//        掛け算
+        public complex mul(complex other){
+            return new complex(real*other.real - imag*other.imag, real* other.imag + imag*other.real);
+        }
+//        割り算
+        public complex div(complex other){
+            return new complex((real*other.real + imag*other.imag)/(other.real*other.real + other.imag*other.imag), (imag*other.real -real*other.imag)/(other.real*other.real + other.imag*other.imag));
+        }
+    }
 }
--- src/Labo2Task03.java
+++ src/Labo2Task03.java
@@ -1,2 +1,86 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Scanner;
+
 public class Labo2Task03 {
+    public static void main(String[] args) {
+        System.out.println("Please input 2 file name.");
+//        ファイル名を取得
+        Scanner scanner = new Scanner(System.in);
+        String fileName1 = scanner.nextLine();
+        String fileName2 = scanner.nextLine();
+//        ファイルがあるか確認
+        try(Scanner scanner_file = new Scanner(new File(fileName1))){
+//            画像を読み込む
+            BufferedImage image1 = ImageIO.read(new File(fileName1));
+            BufferedImage image2 = ImageIO.read(new File(fileName2));
+//            画像のサイズを取得
+            int width1 = image1.getWidth();
+            int height1 = image1.getHeight();
+            System.out.println("("+ width1 + "," + height1 + ")");
+
+            int width2 = image2.getWidth();
+            int height2 = image2.getHeight();
+            System.out.println("("+ width2 + "," + height2 + ")");
+            //        TODO: 画像の距離を出す
+            ImageHistogram histogram1 = new ImageHistogram();
+            ImageHistogram histogram2 = new ImageHistogram();
+
+            histogram1.addImage(image1);
+            histogram2.addImage(image2);
+
+            double distance = histogram1.distance(histogram2);
+            System.out.println("Distance: " + distance);
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    static class ImageHistogram{
+        public int[] counts = new int[64];
+
+        public void addRGB(int rgb){
+            Color color = new Color(rgb);
+            int red = color.getRed();
+            int green = color.getGreen();
+            int blue = color.getBlue();
+
+            int index = 16* (red / 64) + 4 * (green / 64) + (blue / 64);
+            counts[index]++;
+        }
+
+        public void addImage(BufferedImage image) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    int rgb = image.getRGB(x, y);
+                    addRGB(rgb);
+                }
+            }
+        }
+
+        public double distance(ImageHistogram other){
+            double sumcounts1 = 0;
+            double sumcounts2 = 0;
+            for (int i = 0; i < counts.length; i++){
+                sumcounts1 += counts[i] ;
+                sumcounts2 += other.counts[i];
+            }
+            double[] p = new double[64];
+            double[] q = new double[64];
+
+            double distance = 0;
+
+            for (int i = 0; i < counts.length; i++){
+                p[i] = counts[i] / sumcounts1;
+                q[i] = other.counts[i] / sumcounts2;
+
+
+                distance =+ Math.abs(p[i] - q[i]);
+
+            }
+            return distance;
+        }
+    }
 }
